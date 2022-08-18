7ff5f67b0b59544d40e5635f0323e555349b3886
NicoUmansky <umanskynico@gmail.com>
2022-08-18 14:11:52 -0300

    Intento de avance nico

--- php/index.php
+++ php/index.php
@@ -11,15 +11,19 @@
   		<a href="registrar.php">Registrarte |</a>
   		<a href="login.php"> Iniciar Sesión</a>
     </div>
-    <div id=searchbtn>
+    <form id="searchbtn" action="">
+      
       <input type="search" name="barrabusqueda" placeholder="¿Qué estabas buscando?" class="barrabusqueda">
-
-
+      
+    </form>
     <div id="publicarbtn" >
       <button class="btnpublicar" onclick="location.href='publicar.php'">+</button>
   </div>
   <a href="index.php" >
   <img id="logofoto" src="../imagenes/Logo.png"  >
+  </a>
+  <a href="">
+    <img id="corazon"src="../imagenes/Corazon.png">
   </a>  
 
 
