608f131d91951c4db9b207aa3b5af76bed2e2d82
OctavioSpano <tutispano@gmail.com>
2022-09-23 19:28:28 -0300

    sigo con el responsive del encabezado1.php y styles1.css

--- php/encabezado1.php
+++ php/encabezado1.php
@@ -7,7 +7,7 @@ session_start();?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" href="../css/materialize.css" />
-    <!--<link rel="stylesheet" href="../css/styles.css" />-->
+    <link rel="stylesheet" href="../css/styles1.css" />
      <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script> 
      <script src="../js/materialize.js" type="text/javascript"></script> 
      <script src="../js/redress.js" type="text/javascript"></script> 
@@ -21,7 +21,7 @@ session_start();?>
     </div>
     <!--<form id="searchbtn" action="" method="">-->
      <div class="row">
-        <div class="col s12 l1">
+        <div class="col s12 l2">
             <a href="index.php"><img src="../imagenes/Logo.png" class="responsive-img"></a>
         </div>
         <div class="col s12 l1">
@@ -32,24 +32,24 @@ session_start();?>
             <input type="text" id= "barrabusqueda" name="barrabusqueda" placeholder="¿Qué estabas buscando?" class="barrabusqueda">	      
         </div>
         <div class="col s4 l1">
-            <a href="pendientes.php">
+            <a href="pendientes.php" id="btnpendientes">
                 <i class="medium material-icons">hourglass_empty</i>
                 <!--<img class="responsive-img" id="pendientes"src="../imagenes/RelojArena.png">-->
               </a>  
         </div>
         <div class="col s4 l1">
-              <a href="notificaciones.php">
+              <a href="notificaciones.php" id="btnnotificaciones">
                 <i class="medium material-icons">notifications</i>
                 <!--<img class="responsive-img" id="campana"src="../imagenes/Campanita.png">-->
             </a>
         </div>
         <div class="col s4 l1">  
-              <a href="localizacion.php">
+              <a href="localizacion.php" id="btnmapa">
                 <i class="medium material-icons">location_on</i>
                 <!--<img class="responsive-img" id="btn_mapa"src="../imagenes/Mapa.png">-->
             </a>
         </div>
-        <div class="col s12 l2">
+        <div class="col s12 l2" class="acceso-usuario">
               <?php
               if (isset($_SESSION['nom'])){
                 echo $_SESSION['nom'];
@@ -65,40 +65,51 @@ session_start();?>
 </a>
 <div class="row">
         <div class="col s4 l1">
-            <a href="buscado1.php" class="waves-effect waves-light btn">Remeras</a>
+            <a href="buscado1.php" id="btnremeras" class="waves-effect waves-light btn">Remeras</a>
 <!--            <a href="buscado1.php">
             <img id="btn_remera"src="../imagenes/Boton Remera.png">
           </a>-->
         </div>
         <div class="col s4 l1">
-            <a href="buscado1.php" class="waves-effect waves-light btn">Pantalones</a>
+            <a href="buscado1.php"  id="btnbuzos" class="waves-effect waves-light btn">Buzos</a>
 <!--            <a href="buscado1.php">
             <img id="btn_buzo"src="../imagenes/Boton hoodies.png">
             </a>-->
         </div>
-  <a href="buscado1.php">
-  <img id="btn_ropinterior"src="../imagenes/Boton ropa interior.png">
-</a>
-  <a href="buscado1.php">
-  <img id="btn_zapas"src="../imagenes/Boton zapatillas.png">
-</a>
-  <a href="buscado1.php">
-  <img id="btn_camperas"src="../imagenes/Boton camperas.png">
-</a>
-  <a href="buscado1.php">
-  <img id="btn_shorts"src="../imagenes/Boton camperas (1).png">
-</a>
-</a>
-  <a href="buscado1.php">
-  <img id="btn_pantalones"src="../imagenes/Boton Pantalones.png">
-</a>
+        <div class="col s4 l1">
+            <a href="buscado1.php" id="btnropaInterior" class="waves-effect waves-light btn">Ropa Interior</a>
+<!--            <a href="buscado1.php">
+            <img id="btn_ropinterior"src="../imagenes/Boton ropa interior.png">
+            </a>-->
+        </div>
+        <div class="col s4 l1">    
+            <a href="buscado1.php" id="btnzapas" class="waves-effect waves-light btn">Zapatillas</a>
+<!--        <a href="buscado1.php">
+            <img id="btn_zapas"src="../imagenes/Boton zapatillas.png">
+            </a>-->
+        </div>
+        <div class="col s4 l1">
+            <a href="buscado1.php" id="btncamperas" class="waves-effect waves-light btn">Camperas</a>
+<!--        <img id="btn_camperas"src="../imagenes/Boton camperas.png">
+            </a>-->
+        </div>
+        <div class="col s4 l1">
+            <a href="buscado1.php" id="btnshorts" class="waves-effect waves-light btn">Shorts</a>
+<!--            <img id="btn_shorts"src="../imagenes/Boton camperas (1).png">
+                </a>-->
+        </div>
+        <div class="col s4 l1">       
+            <a href="buscado1.php" id="btnpantalones" class="waves-effect waves-light btn">Pantalones</a>
+<!--          <img id="btn_pantalones"src="../imagenes/Boton Pantalones.png">
+              </a>-->
+        </div>
 </div>
 <!--<button value="remera" onclick="atajosfunc() "id="btnRem">
   <img id="btnremera"src="../imagenes/Boton Remera.png">
 </button>
 !-->
 <div id="publicarbtn" >
-      <a href="publicar.php" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+      <a href="publicar.php" id="btnpublicar" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons" id="btnpublicar">add</i></a>
       <!--<button class="btnpublicar" onclick="location.href='publicar.php'">+</button>-->
   </div>
   
