6fe37c21d13a4ca2f706d532e32066f4e8815f6f
OctavioSpano <101335584+OctavioSpano@users.noreply.github.com>
2022-10-03 13:36:16 -0300

    homescreen

---
+++ php/homeScreen.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+include("encabezado1.php");
+?>
+<div id="prendascont">
+	
+
+</div>
+
+</html>
--- php/index.php
+++ php/index.php
@@ -1,11 +1,19 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>ReDress</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
 
-include("encabezado1.php");
-?>
-<div id="prendascont">
-	
-
-</div>
-
+    <link rel="stylesheet" href="../css/styles.css" />
+     <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script> 
+    
+     <script src="../js/redress.js" type="text/javascript"></script>
+     <link rel="icon" href="../imagenes/Logo.png"> 
+  </head>
+<body>
+	<a href="../php/homeScreen.php">
+		<img src="../imagenes/Logo.png" id="logoIndex">
+		<label id="lblSlogan" >Reduce. ReDress. Recicle</label>
+</body>
 </html>
