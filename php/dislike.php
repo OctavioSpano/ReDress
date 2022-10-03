65e8724ce4e74eedaec6e798c77731b22b882615
NicoUmansky <umanskynico@gmail.com>
2022-10-03 11:58:36 -0300

    anda perf el dislike y toy haciendo el mail

--- php/dislike.php
+++ php/dislike.php
@@ -6,7 +6,7 @@ echo ($publi);
 
 $con = mysqli_connect("localhost", "root", "rootroot", "redressbd");
 $consulta = "DELETE FROM pendientes WHERE IDUsuario = ".$usuario." AND IDPublicacion = ".$publi."";
-echo $consulta;
+//echo $consulta;
 $resultado=mysqli_query($con, $consulta);
  
 
--- php/pendientes.php
+++ php/pendientes.php
@@ -12,10 +12,11 @@ if(!isset($_SESSION['idu'])){
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" href="../css/styles.css" />
-    <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script> 
+   <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script> 
     <script src="../js/redress.js" type="text/javascript"></script>
 
   </head>
+  <body>
   <a href="index.php" >
     <img id="flechaatras" src="../imagenes/flechaatras.png"  >
     </a>
@@ -25,7 +26,7 @@ if(!isset($_SESSION['idu'])){
   session_start();
   $usuario = $_SESSION['idu'];
   $con = mysqli_connect("localhost", "root", "rootroot", "redressbd");
-  $consulta = "SELECT IDPublicacion from pendientes where IDUsuario = '$usuario'";
+  $consulta = "SELECT IDPublicacion,ID from pendientes where IDUsuario = '$usuario'";
   $res=$con->query($consulta);
 $i=0;
 $izq = 0;
@@ -36,19 +37,24 @@ while ($row = $res->fetch_assoc()) {
   $consulta2= "SELECT *,u.Nombre,u.Apellido FROM prendas p INNER JOIN usuarios u ON p.IDUsuario=u.IDUsuario where IDPublicacion=".$elegido."";
 
   $res2=mysqli_query($con, $consulta2);
-
+$sal='';
   if($res2->num_rows > 0){
       while($datos = $res2->fetch_assoc()){
             //$data['status']='ok';
               $data[$i] = $datos;
-              echo "<div class='cardcont'style='margin-left:".$izq."%;height:250px;width:250px;' >";
-              echo "<h2>".$data[$i]['TipoPrenda']."</h2>";
-              echo "<h3>".$data[$i]['Nombre']." ".$data[$i]['Apellido']."</h2>";
-              echo "<img class='responsive-img' src=".$data[$i]['RutaFoto']." >";
-              echo "<button id='".$data[$i]['IDPublicacion']."'class='removefav'></button>";
-              echo "</div>";
+              $sal.= "<div class='cardcont'style='margin-left:".$izq."%;height:250px;width:250px;' id=".$row['ID']." >";
+              $sal.="<h2>".$data[$i]['TipoPrenda']."</h2>";
+              $sal.="<h3>".$data[$i]['Nombre']." ".$data[$i]['Apellido']."</h2>";
+              $sal.= "<img class='responsive-img' src=".$data[$i]['RutaFoto']." >";
+              $sal.= "<button id='".$data[$i]['IDPublicacion']."|".$row['ID']."' class='removefav'></button>";
+              $sal.= "</div>";
           $i++;
           $izq+=20;
       }
+      echo $sal;
   }
 }
+?>
+</body>
+ 
+</html>
